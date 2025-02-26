26bdab458b37534c33513aeb8d936d36bee0803e
jarari <hazardhax11@gmail.com>
2025-02-27 06:02:48 +0900

    implemented impact effect pooling

--- Assets/Scripts/System/Managers/ImpactManager.cs
+++ Assets/Scripts/System/Managers/ImpactManager.cs
@@ -1,8 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Pool;
 using UnityEngine.VFX;
+
+public class  VFXAliveCheck : MonoBehaviour {
+    public IObjectPool<VisualEffect> pool;
+
+    private VisualEffect _vfx;
+    private void Start() {
+        _vfx = GetComponent<VisualEffect>();
+    }
+
+    private void Update() {
+        if (!_vfx.HasAnySystemAwake()) {
+            pool.Release(_vfx);
+        }
+    }
+}
 
 public class ImpactManager : MonoBehaviour {
     public static ImpactManager instance;
@@ -12,11 +28,9 @@ public class ImpactManager : MonoBehaviour {
         public VisualEffectAsset effectAsset;
     }
     public int maxPoolSize = 1000;
-    public List<Impact> bulletImpacts = new List<Impact>();
-    public List<Impact> meleeImpacts = new List<Impact>();
+    public List<Impact> impacts = new List<Impact>();
 
-    private Dictionary<PhysicMaterial, VisualEffectAsset> _bulletImpactDict = new Dictionary<PhysicMaterial, VisualEffectAsset>();
-    private Dictionary<PhysicMaterial, VisualEffectAsset> _meleeImpactDict = new Dictionary<PhysicMaterial, VisualEffectAsset>();
+    private Dictionary<PhysicMaterial, VisualEffectAsset> _impactDict = new Dictionary<PhysicMaterial, VisualEffectAsset>();
     IObjectPool<VisualEffect> _pool; 
     public IObjectPool<VisualEffect> Pool {
         get {
@@ -34,15 +48,9 @@ public class ImpactManager : MonoBehaviour {
         }
         instance = this;
 
-        foreach(var impact in bulletImpacts) {
-            if (!_bulletImpactDict.ContainsKey(impact.material)) {
-                _bulletImpactDict.Add(impact.material, impact.effectAsset);
-            }
-        }
-
-        foreach (var impact in meleeImpacts) {
-            if (!_meleeImpactDict.ContainsKey(impact.material)) {
-                _meleeImpactDict.Add(impact.material, impact.effectAsset);
+        foreach(var impact in impacts) {
+            if (!_impactDict.ContainsKey(impact.material)) {
+                _impactDict.Add(impact.material, impact.effectAsset);
             }
         }
     }
@@ -51,11 +59,15 @@ public class ImpactManager : MonoBehaviour {
         var go = new GameObject("Pooled VFX");
         var vfx = go.AddComponent<VisualEffect>();
         vfx.Stop();
+
+        var aliveCheck = go.AddComponent<VFXAliveCheck>();
+        aliveCheck.pool = Pool;
         return vfx;
     }
 
     private void OnReturnedToPool(VisualEffect vfx) {
         vfx.gameObject.SetActive(false);
+        Debug.Log("Returned to pool: " + vfx.name);
     }
 
     private void OnTakeFromPool(VisualEffect vfx) {
@@ -67,6 +79,15 @@ public class ImpactManager : MonoBehaviour {
     }
 
     public void SpawnImpactEffect(Vector3 position, Vector3 normal, PhysicMaterial material = null) {
-
+        VisualEffect vfx = Pool.Get();
+        vfx.transform.position = position;
+        vfx.transform.up = normal;
+        if (material != null && _impactDict.TryGetValue(material, out var asset)) {
+            vfx.visualEffectAsset = asset;
+        }
+        else {
+            vfx.visualEffectAsset = _impactDict.ElementAt(0).Value;
+        }
+        vfx.Play();
     }
 }
