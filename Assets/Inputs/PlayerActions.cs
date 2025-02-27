f69fd6f57918d2d296d7283236a71096c1c8feba
jarari <hazardhax11@gmail.com>
2025-02-27 15:45:12 +0900

    added initial check to fire

--- Assets/Inputs/PlayerActions.cs
+++ Assets/Inputs/PlayerActions.cs
@@ -61,7 +61,7 @@ public partial class @PlayerActions: IInputActionCollection2, IDisposable
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """",
-                    ""initialStateCheck"": false
+                    ""initialStateCheck"": true
                 },
                 {
                     ""name"": ""Aim"",
