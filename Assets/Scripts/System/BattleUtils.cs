56ba6df9a9e4c18e3a15b35aa6570612159e319f
jarari <hazardhax11@gmail.com>
2025-03-06 16:24:04 +0900

    implemented basic damage calculation

---
+++ Assets/Scripts/System/BattleUtils.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BattleUtils {
+    public static float CalculateDamage(Character attacker, Character victim, float damageMult, bool canCrit) {
+        float victimDef = (victim.GetFinalStat(StatType.Defense) - attacker.GetFinalStat(StatType.DefFlatPenetration)) * (100 - attacker.GetFinalStat(StatType.DefPercentagePenetration));
+        float baseDamage = attacker.GetFinalStat(StatType.Attack) * damageMult;
+        if (canCrit && Random.Range(float.Epsilon, 100) <= attacker.GetFinalStat(StatType.CritChance)) {
+            baseDamage *= attacker.GetFinalStat(StatType.CritMult) / 100f;
+        }
+        float damageReduction = victimDef / (victimDef + 100f);
+        return baseDamage * (1f - damageReduction);
+    }
+
+    public static void DoDamage(Character victim, float damage) {
+        victim.data.stats.CurrentHP -= damage;
+        if (victim.data.stats.CurrentHP <= 0) {
+            victim.Kill();
+        }
+    }
+}
