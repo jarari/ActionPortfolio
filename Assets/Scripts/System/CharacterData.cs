e47e6f535f9a842ea4ed880e99d17443957ea3b0
jarari <hazardhax11@gmail.com>
2025-03-06 15:03:35 +0900

    restructured for stat modifier

--- Assets/Scripts/System/CharacterData.cs
+++ Assets/Scripts/System/CharacterData.cs
@@ -5,12 +5,27 @@ public enum CharacterType {
     Shooter
 }
 
+public enum StatType {
+    CurrentHP,
+    MaxHP,
+    Attack,
+    Defense,
+    CritChance,
+    CritMult,
+    DefFlatPenetration,
+    DefPercentagePenetration
+}
+
 [System.Serializable]
 public struct CharacterStats {
-    public float CurrentHP;
-    public float MaxHP;
-    public float Attack;
-    public float Defense;
+    public float CurrentHP { get; set; }
+    public float MaxHP { get; set; }
+    public float Attack { get; set; }
+    public float Defense { get; set; }
+    public float CritChance { get; set; }
+    public float CritMult { get; set; }
+    public float DefFlatPenetration { get; set; }
+    public float DefPercentagePenetration { get; set; }
 }
 
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/CharacterData", order = 1)]
