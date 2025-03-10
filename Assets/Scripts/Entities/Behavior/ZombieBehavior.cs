9682efa13f66187a4e6166dde3a9c8e7c971229a
jarari <hazardhax11@gmail.com>
2025-03-10 16:55:50 +0900

    wip zombie behavior

--- Assets/Scripts/Entities/Behavior/ZombieBehavior.cs
+++ Assets/Scripts/Entities/Behavior/ZombieBehavior.cs
@@ -1,32 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 [RequireComponent(typeof(Character))]
 public class ZombieBehavior : MonoBehaviour {
     enum ZombieState {
         Idle,
-        Alerted,
         Chase,
+        Attacking,
         Ragdoll,
         Dead
     }
 
-    const float alertToIdleTime = 5f;
+    public bool IsStateLocked { get; private set; }
 
     [SerializeField]
-    private float _searchDistanceIdle = 10f;
-    private float _alertTimer = 0f;
+    private float _searchDistance = 10f;
+    private float _searchPeriod = 0.2f;
+    private float _searchTimer = 0f;
+    private float _searchAngle = 30f * Mathf.Deg2Rad;
+
+    private float _circlingRadius = 3f;
+    private int _circlingDir = 1;
+    private float _attackRange = 1.5f;
+    private float _attackDelay = 12f;
+    private float _nextAttack = 0f;
 
     Character _character;
     Animator _animator;
+    NavMeshAgent _agent;
     Rigidbody _rigidbody;
     Transform _headBone;
     ZombieState _currentState;
-    Transform _chasingTarget;
+    GameObject _chasingTarget;
 
     private void Awake() {
         _character = GetComponent<Character>();
+        _agent = GetComponent<NavMeshAgent>();
         _animator = GetComponent<Animator>();
         _rigidbody = GetComponent<Rigidbody>();
         _headBone = _animator.GetBoneTransform(HumanBodyBones.Head);
@@ -35,28 +46,93 @@ public class ZombieBehavior : MonoBehaviour {
         _character.OnStandUp += OnStandUp;
         _character.OnDeath += OnDeath;
         _character.OnHit += OnHit;
+        _character.OnAttackEnd += OnAttackEnd;
+        _nextAttack = Time.time;
+
+        _animator.applyRootMotion = true;
+        _agent.updatePosition = false;
+        _agent.updateRotation = false;
+        _agent.SetDestination(transform.position);
+    }
+
+    private void OnAnimatorMove() {
+        Vector3 rootPosition = _animator.rootPosition;
+        transform.position = _animator.rootPosition;
+        transform.rotation = _animator.rootRotation;
+        _agent.nextPosition = rootPosition;
     }
 
     private void Search() {
-
+        var charList = CharacterManager.instance.GetCharactersInRangeByTeam(transform.position, _searchDistance, Character.CharacterTeam.Player);
+        if (charList.Count > 0) {
+            float minDist = float.MaxValue;
+            foreach (var c in charList) {
+                float ang = Mathf.Acos(Vector3.Dot(_headBone.forward, (c.GetHips().position - _headBone.position).normalized));
+                float dist = c.GetDistanceFromBumper(transform.position);
+                if (minDist > dist && ang <= _searchAngle) {
+                    minDist = dist;
+                    _chasingTarget = c.gameObject;
+                }
+            }
+            if (_chasingTarget != null) {
+                _animator.SetTrigger("Scream");
+                _currentState = ZombieState.Chase;
+            }
+        }
     }
 
     private void IdleBehavior() {
+        _searchTimer += Time.deltaTime;
+        if (_searchTimer >= _searchPeriod) {
+            _searchTimer = 0f;
+            Search();
+        }
     }
 
     private void OnDrawGizmosSelected() {
         Gizmos.color = Color.red;
-        Gizmos.DrawSphere(GetComponent<Animator>().GetBoneTransform(HumanBodyBones.Head).position, _searchDistanceIdle);
-    }
-
-    private void AlertedBehavior() {
-        _alertTimer += Time.deltaTime;
-        if (_alertTimer >= alertToIdleTime) {
-            _currentState = ZombieState.Idle;
+        if (_agent != null) {
+            Gizmos.DrawSphere(_agent.nextPosition, 0.1f);
         }
     }
 
     private void ChaseBehavior() {
+        if (_chasingTarget.CompareTag("Character")) {
+            Character targetCharacter = _chasingTarget.GetComponent<Character>();
+            _agent.SetDestination(_chasingTarget.transform.position);
+            Vector3 diff = (_agent.nextPosition - transform.position);
+            diff.y = 0f;
+            Vector3 diffNorm = diff.normalized;
+            if (Time.time >= _nextAttack) {
+                if (targetCharacter.GetDistanceFromBumper(transform.position) > _attackRange) {
+                    _character.SetDesiredDirection(diffNorm.x, diffNorm.z);
+                    _character.SetSprint(true);
+                }
+                else {
+                    _character.SetDesiredDirection(0, 0);
+                    _animator.SetTrigger("Punch");
+                    _currentState = ZombieState.Attacking;
+                    _nextAttack = Time.time + _attackDelay;
+                    if (Random.value < 0.5f) {
+                        _circlingDir = -1;
+                    }
+                    else {
+                        _circlingDir = 1;
+                    }
+                }
+            }
+            else {
+                Vector3 targetPos = _chasingTarget.transform.position - Quaternion.AngleAxis(5f * _circlingDir, Vector3.up) * diffNorm * _circlingRadius;
+                _agent.SetDestination(targetPos);
+                Vector3 moveDir = (_agent.nextPosition - transform.position).normalized;
+                _character.SetDesiredDirection(moveDir.x, moveDir.z);
+                _character.SetSprint(false);
+            }
+            _character.SetDesiredAimTo(targetCharacter.GetHead().position);
+        }
+    }
+
+    private void AttackBehavior() {
 
     }
 
@@ -69,7 +145,7 @@ public class ZombieBehavior : MonoBehaviour {
             _currentState = ZombieState.Chase;
         }
         else {
-            _currentState = ZombieState.Alerted;
+            _currentState = ZombieState.Idle;
         }
     }
 
@@ -78,8 +154,14 @@ public class ZombieBehavior : MonoBehaviour {
     }
 
     private void OnHit(Character attacker, DamageType type, float damage) {
+        if (_currentState != ZombieState.Chase) {
+            _currentState = ZombieState.Chase;
+            _chasingTarget = attacker.gameObject;
+        }
+    }
+
+    private void OnAttackEnd() {
         _currentState = ZombieState.Chase;
-        _chasingTarget = attacker.transform;
     }
 
     void Update() {
@@ -87,12 +169,16 @@ public class ZombieBehavior : MonoBehaviour {
             case ZombieState.Idle:
                 IdleBehavior();
                 break;
-            case ZombieState.Alerted:
-                AlertedBehavior();
-                break;
             case ZombieState.Chase:
                 ChaseBehavior();
                 break;
+            case ZombieState.Attacking:
+                AttackBehavior();
+                break;
         }
+    }
+
+    public void SetChasingTarget(GameObject target) {
+        _chasingTarget = target;
     }
 }
